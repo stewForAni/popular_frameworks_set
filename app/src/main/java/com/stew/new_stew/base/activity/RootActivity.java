da9f3816275966a64583608d8745f40f737d4048
stew <stewforani@gmail.com>
2018-12-06 18:41:38 +0800

    add statusbarutil file and logo icon

--- app/src/main/java/com/stew/new_stew/base/activity/RootActivity.java
+++ app/src/main/java/com/stew/new_stew/base/activity/RootActivity.java
@@ -1,9 +1,13 @@
 package com.stew.new_stew.base.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+
+import com.stew.new_stew.R;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -22,7 +26,9 @@ public abstract class RootActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, TAG + "onCreate");
+        setStatusBar();
         setContentView(getLayoutID());
+
         /*
          * bind butterKnife at RootActivity onCreate()
          * and unbind at onDestroy()
@@ -39,6 +45,13 @@ public abstract class RootActivity extends AppCompatActivity {
         if (unbinder != null && unbinder != Unbinder.EMPTY) {
             unbinder.unbind();
             unbinder = null;
+        }
+    }
+
+    private void setStatusBar() {
+        getWindow().setStatusBarColor(getResources().getColor(R.color.status_color_def));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
 
