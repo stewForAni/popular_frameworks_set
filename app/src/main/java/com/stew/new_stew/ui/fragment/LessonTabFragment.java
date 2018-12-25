d9b49a729911c31458ff55dff338e0466d2a32ae
stew <stewforani@gmail.com>
2018-12-25 18:28:52 +0800

    add fragments

---
+++ app/src/main/java/com/stew/new_stew/ui/fragment/LessonTabFragment.java
@@ -0,0 +1,11 @@
+package com.stew.new_stew.ui.fragment;
+
+import com.stew.new_stew.base.fragment.RootFragment;
+
+/**
+ * describe:
+ * date： 2018/12/25 on 18:23
+ * author: stew (https://github.com/stewForAni)
+ */
+public class LessonTabFragment extends RootFragment {
+}
