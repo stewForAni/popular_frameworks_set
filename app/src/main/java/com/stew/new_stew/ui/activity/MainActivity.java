da257a6f17d9777bb78a4daabe77dd7edf2167c5
stew <stewforani@gmail.com>
2018-12-27 14:56:13 +0800

    add tabbarlayout icon and code

--- app/src/main/java/com/stew/new_stew/ui/activity/MainActivity.java
+++ app/src/main/java/com/stew/new_stew/ui/activity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.stew.new_stew.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.stew.new_stew.R;
 import com.stew.new_stew.base.activity.BaseActivity;
@@ -14,6 +20,7 @@ import com.stew.new_stew.ui.adapter.MainPageViewAdapter;
 import com.stew.new_stew.ui.fragment.LessonTabFragment;
 import com.stew.new_stew.ui.fragment.MainTabFragment;
 import com.stew.new_stew.ui.fragment.MeTabFragment;
+import com.stew.new_stew.utils.CommonVarUtil;
 import com.stew.new_stew.utils.DeviceUtil;
 
 import java.util.ArrayList;
@@ -26,15 +33,34 @@ import butterknife.BindView;
  * date： 2018/11/28 on 13:24
  * author: stew (https://github.com/stewForAni)
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getName();
+    private int clickTabID;
 
     @BindView(R.id.main_viewpager)
     ViewPager mainViewpager;
+    @BindView(R.id.top_bar_ll)
+    LinearLayout topBarLayout;
+    @BindView(R.id.main_tab_ll_home)
+    LinearLayout mainTabLlHome;
+    @BindView(R.id.main_tab_ll_lesson)
+    LinearLayout mainTabLlLesson;
+    @BindView(R.id.main_tab_ll_me)
+    LinearLayout mainTabLlMe;
+    @BindView(R.id.main_tab_iv_home)
+    ImageView mainTabIvHome;
+    @BindView(R.id.main_tab_iv_lesson)
+    ImageView mainTabIvLesson;
+    @BindView(R.id.main_tab_iv_me)
+    ImageView mainTabIvMe;
+    @BindView(R.id.main_tab_tx_home)
+    TextView mainTabTxHome;
+    @BindView(R.id.main_tab_tx_lesson)
+    TextView mainTabTxLesson;
+    @BindView(R.id.main_tab_tx_me)
+    TextView mainTabTxMe;
 
-    @BindView(R.id.top_bar_rl)
-    RelativeLayout topBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +84,7 @@ public class MainActivity extends BaseActivity {
         Log.d(TAG, "initMain: ");
 
         initTopBarLayout();
+        initBottomBarLayout();
 
         List<Fragment> fragmentList = new ArrayList<>();
         fragmentList.add(new MainTabFragment());
@@ -89,10 +116,61 @@ public class MainActivity extends BaseActivity {
     private void initTopBarLayout() {
         RelativeLayout relativeLayout = new RelativeLayout(this);
         int topBarLayoutHeight = DeviceUtil.getStatusBarHeight() + DeviceUtil.dip2px(this, 46);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,topBarLayoutHeight);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, topBarLayoutHeight);
         relativeLayout.setLayoutParams(params);
         relativeLayout.setBackgroundResource(R.drawable.top_layout_gradient);
-        topBarLayout.addView(relativeLayout);
+        topBarLayout.addView(relativeLayout, 0);
+    }
+
+    private void initBottomBarLayout() {
+        mainTabLlHome.setOnClickListener(this);
+        mainTabLlLesson.setOnClickListener(this);
+        mainTabLlMe.setOnClickListener(this);
+        onClick(mainTabLlHome);
+        clickTabID = R.id.main_tab_ll_home;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (clickTabID == v.getId()) {
+            Log.d(TAG, "click repetition");
+            return;
+        } else {
+            clickTabID = v.getId();
+        }
+
+        initIcon();
+
+        switch (v.getId()) {
+
+            case R.id.main_tab_ll_home: {
+                mainTabIvHome.setImageResource(R.drawable.main_tab_icon_home_selected);
+                mainTabTxHome.setTextSize(TypedValue.COMPLEX_UNIT_SP, CommonVarUtil.MAIN_TAB_TEXT_SIZE_SELECTED);
+                break;
+            }
+
+            case R.id.main_tab_ll_lesson: {
+                mainTabIvLesson.setImageResource(R.drawable.main_tab_icon_lesson_selected);
+                mainTabTxLesson.setTextSize(TypedValue.COMPLEX_UNIT_SP, CommonVarUtil.MAIN_TAB_TEXT_SIZE_SELECTED);
+                break;
+            }
+
+            case R.id.main_tab_ll_me: {
+                mainTabIvMe.setImageResource(R.drawable.main_tab_icon_me_selected);
+                mainTabTxMe.setTextSize(TypedValue.COMPLEX_UNIT_SP, CommonVarUtil.MAIN_TAB_TEXT_SIZE_SELECTED);
+                break;
+            }
+        }
+    }
+
+    private void initIcon() {
+        mainTabIvHome.setImageResource(R.drawable.main_tab_icon_home);
+        mainTabIvLesson.setImageResource(R.drawable.main_tab_icon_lesson);
+        mainTabIvMe.setImageResource(R.drawable.main_tab_icon_me);
+        mainTabTxHome.setTextSize(TypedValue.COMPLEX_UNIT_SP, CommonVarUtil.MAIN_TAB_TEXT_SIZE);
+        mainTabTxLesson.setTextSize(TypedValue.COMPLEX_UNIT_SP, CommonVarUtil.MAIN_TAB_TEXT_SIZE);
+        mainTabTxMe.setTextSize(TypedValue.COMPLEX_UNIT_SP, CommonVarUtil.MAIN_TAB_TEXT_SIZE);
     }
 
     @Override
---
+++ app/src/main/java/com/stew/new_stew/utils/CommonVarUtil.java
@@ -0,0 +1,11 @@
+package com.stew.new_stew.utils;
+
+/**
+ * describe:
+ * date： 2018/12/27 on 14:52
+ * author: stew (https://github.com/stewForAni)
+ */
+public class CommonVarUtil {
+    public static final int MAIN_TAB_TEXT_SIZE = 12;
+    public static final int MAIN_TAB_TEXT_SIZE_SELECTED = 15;
+}
